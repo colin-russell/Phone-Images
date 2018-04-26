cd7f23c1b8d0666af83de2da5297452dc6c4c42c
Colin Russell <colinrussell101@gmail.com>
2018-04-26 12:03:01 -0400

    final commit --forgot to do a second commit

--- Phone Images/ViewController.m
+++ Phone Images/ViewController.m
@@ -9,21 +9,46 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property UIImageView *iPhoneImageView;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self setupImageView];
+    
+    NSURL *url = [NSURL URLWithString:@"http://imgur.com/y9MIaCS.png"]; // step 1
+    
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration]; // step 2
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration]; // step 3
+    
+    NSURLSessionDownloadTask *downloadTask = [session downloadTaskWithURL:url completionHandler:^(NSURL * _Nullable location, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        if (error) { // 1
+            // Handle the error
+            NSLog(@"error: %@", error.localizedDescription);
+            return;
+        }
+        
+        NSData *data = [NSData dataWithContentsOfURL:location];
+        UIImage *image = [UIImage imageWithData:data]; // 2
+        
+        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+            // This will run on the main queue
+            
+            self.iPhoneImageView.image = image; // 4
+        }];
+    }]; // step 4
+    
+    [downloadTask resume]; // step 5
+
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)setupImageView {
+    self.iPhoneImageView = [[UIImageView alloc] initWithFrame:self.view.bounds];
+    [self.view addSubview:self.iPhoneImageView];
 }
-
 
 @end
